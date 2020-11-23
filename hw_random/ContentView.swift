c3a635a39eeeff87fb57e192b75cf9c9cf5cf5d8
Patty <L224749391@gmail.com>
2020-11-23 10:16:07 +0800

    Initial Commit

---
+++ hw_random/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw_random
+//
+//  Created by 林湘羚 on 2020/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hw_random/hw_randomApp.swift
@@ -0,0 +1,17 @@
+//
+//  hw_randomApp.swift
+//  hw_random
+//
+//  Created by 林湘羚 on 2020/11/23.
+//
+
+import SwiftUI
+
+@main
+struct hw_randomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
